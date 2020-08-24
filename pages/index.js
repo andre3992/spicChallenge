d9f25d4d371a09e45b6e644dce45a0b80342d5ca
unknown <AndreRegedor@HPNX7400.implexo.local>
2020-08-24 15:39:38 +0100

    changes to get information from database

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import MenuLateral from "../components/MenuLateral/menuLateral";
 import TopBar from "../components/TopBar/topBar";
+import fetch from "isomorphic-unfetch";
 
-export default function Index() {
+const Index = ({ data }) => {
+  /*  const [results, setResults] = useState(data);
+
+  async function getStaticProps(context) {
+    const res = await fetch("http://localhost:3000/api/daily");
+    const json = await res.json();
+    return (
+      <div>
+        <p> ola </p>
+        {{results.name}}
+      </div>
+    );
+  }
+};
+*/
+  const [results, setResults] = useState(data);
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <TopBar></TopBar>
+        {results.name}
         <MenuLateral></MenuLateral>
       </Box>
     </Container>
   );
-}
+};
+Index.getInitialProps = async () => {
+  const res = await fetch("http://localhost:3000/api/guests");
+  const json = await res.json();
+  return { data: json };
+};
+
+export default Index;
